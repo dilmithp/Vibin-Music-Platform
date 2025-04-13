517281a60178afdd9c61b57ee976544694478534
dilmithp <dilmithp@outlook.com>
2025-04-13 16:05:23 +0530

    add-db-config-files

---
+++ src/main/java/com/vibin/util/ConnectionData.java
@@ -0,0 +1,8 @@
+package com.vibin.util;
+
+public class ConnectionData {
+
+    public static final String DATABASE = "vibin";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "";
+}
