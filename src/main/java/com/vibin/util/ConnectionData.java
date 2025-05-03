7826468955547277ab5471535e3246dfc8a05de5
dilmithp <dilmithp@outlook.com>
2025-05-03 19:44:51 +0530

    songlist

--- src/main/java/com/vibin/util/ConnectionData.java
+++ src/main/java/com/vibin/util/ConnectionData.java
@@ -4,5 +4,5 @@ public class ConnectionData {
 
     public static final String DATABASE = "vibin";
     public static final String USERNAME = "root";
-    public static final String PASSWORD = "";
+    public static final String PASSWORD = "root";
 }
